45ce84d5d8eaac78ce4ab4d9bd4e949570797dab
UlumBahrul <203040089@mail.unpas.ac.id>
2022-10-21 22:21:36 +0700

    blade templating engine

--- laravel_203040089/routes/web.php
+++ laravel_203040089/routes/web.php
@@ -14,17 +14,67 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "home"
+    ]);
 });
 
 Route::get('/about', function () {
-    return view('about' , [
+    return view('about', [
+        "title" => "About",
         "name" => "Bahrul Ulum",
         "email" => "203040089@mail.unpas.ac.id",
         "image" => "a.jpg"
     ]);
 });
 
-Route::get('/blog', function () {
-    return view('blog');
+
+
+Route::get('/posts', function () {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Bahrul Ullum",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, facere! Voluptates a rem est officia nisi odit qui, reprehenderit molestias! Neque inventore molestiae mollitia itaque tenetur, voluptate similique suscipit tempora aperiam dolorum iusto placeat veritatis animi labore illo minus dolorem nesciunt, dolore veniam. Quae obcaecati numquam accusantium qui? Non distinctio, atque nihil rerum vero veritatis sint, dignissimos ducimus minima consectetur debitis amet libero fugit consequuntur inventore hic? Natus autem fugit pariatur temporibus, repellat eos? Sit animi quos dignissimos quibusdam alias."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Agung Septiana",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, facere! Voluptates a rem est officia nisi odit qui, reprehenderit molestias! Neque inventore molestiae mollitia itaque tenetur, voluptate similique suscipit tempora aperiam dolorum iusto placeat veritatis animi labore illo minus dolorem nesciunt, dolore veniam. Quae obcaecati numquam accusantium qui? Non distinctio, atque nihil rerum vero veritatis sint, dignissimos ducimus minima consectetur debitis amet libero fugit consequuntur inventore hic? Natus autem fugit pariatur temporibus, repellat eos? Sit animi quos dignissimos quibusdam alias.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, facere! Voluptates a rem est officia nisi odit qui, reprehenderit molestias! Neque inventore molestiae mollitia itaque tenetur, voluptate similique suscipit tempora aperiam dolorum iusto placeat veritatis animi labore illo minus dolorem nesciunt, dolore veniam. Quae obcaecati numquam accusantium qui? Non distinctio, atque nihil rerum vero veritatis sint, dignissimos ducimus minima consectetur debitis amet libero fugit consequuntur inventore hic? Natus autem fugit pariatur temporibus, repellat eos? Sit animi quos dignissimos quibusdam alias."
+        ]
+    ];
+    return view('posts', [
+        "title" => "Post", "posts" => $blog_posts
+    ]);
+});
+
+Route::get('posts/{slug}', function ($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Bahrul Ullum",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, facere! Voluptates a rem est officia nisi odit qui, reprehenderit molestias! Neque inventore molestiae mollitia itaque tenetur, voluptate similique suscipit tempora aperiam dolorum iusto placeat veritatis animi labore illo minus dolorem nesciunt, dolore veniam. Quae obcaecati numquam accusantium qui? Non distinctio, atque nihil rerum vero veritatis sint, dignissimos ducimus minima consectetur debitis amet libero fugit consequuntur inventore hic? Natus autem fugit pariatur temporibus, repellat eos? Sit animi quos dignissimos quibusdam alias."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Agung Septiana",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, facere! Voluptates a rem est officia nisi odit qui, reprehenderit molestias! Neque inventore molestiae mollitia itaque tenetur, voluptate similique suscipit tempora aperiam dolorum iusto placeat veritatis animi labore illo minus dolorem nesciunt, dolore veniam. Quae obcaecati numquam accusantium qui? Non distinctio, atque nihil rerum vero veritatis sint, dignissimos ducimus minima consectetur debitis amet libero fugit consequuntur inventore hic? Natus autem fugit pariatur temporibus, repellat eos? Sit animi quos dignissimos quibusdam alias.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, facere! Voluptates a rem est officia nisi odit qui, reprehenderit molestias! Neque inventore molestiae mollitia itaque tenetur, voluptate similique suscipit tempora aperiam dolorum iusto placeat veritatis animi labore illo minus dolorem nesciunt, dolore veniam. Quae obcaecati numquam accusantium qui? Non distinctio, atque nihil rerum vero veritatis sint, dignissimos ducimus minima consectetur debitis amet libero fugit consequuntur inventore hic? Natus autem fugit pariatur temporibus, repellat eos? Sit animi quos dignissimos quibusdam alias."
+        ]
+    ];
+
+    $new_post = [];
+    foreach ($blog_posts as $post) {
+        if ($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
