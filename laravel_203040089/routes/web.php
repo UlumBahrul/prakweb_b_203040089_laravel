479e28ec1a56e33cd33f541ae189bece21033927
UlumBahrul <114681101+UlumBahrul@users.noreply.github.com>
2022-10-21 15:07:05 +0700

    struktur folder, routes & view laravel

--- laravel_203040089/routes/web.php
+++ laravel_203040089/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about' , [
+        "name" => "Bahrul Ulum",
+        "email" => "203040089@mail.unpas.ac.id",
+        "image" => "a.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('post');
 });
