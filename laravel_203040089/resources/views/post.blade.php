841cbb7a9ad9f547464b1d78d91737102edc9a74
UlumBahrul <203040089@mail.unpas.ac.id>
2022-10-30 01:56:32 +0700

    video 8. post category & eloquent relationship

--- laravel_203040089/resources/views/post.blade.php
+++ laravel_203040089/resources/views/post.blade.php
@@ -3,9 +3,11 @@
 @section('container')
     <article class="mb-4">
         <h2 class="mb-4">{{ $post->title }}</h2>
-    
+        
+        <p>By. Bahrul ulum in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+
         {!! $post->body !!}
     </article>
 
-    <a href="/posts/" >Back to Posts</a>
+    <a href="/posts" >Back to Posts</a>
 @endsection
