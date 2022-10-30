f83ef2e047a98cb37f2d7f8dd135d3e1be8654c1
UlumBahrul <203040089@mail.unpas.ac.id>
2022-10-30 22:16:00 +0700

    video 11. N+1 problem

--- laravel_203040089/routes/web.php
+++ laravel_203040089/routes/web.php
@@ -18,18 +18,18 @@ use App\Models\User;
 */
 
 Route::get('/', function () {
-    return view('home', [
-        "title" => "home"
-    ]);
+  return view('home', [
+    "title" => "home"
+  ]);
 });
 
 Route::get('/about', function () {
-    return view('about', [
-        "title" => "About",
-        "name" => "Bahrul Ulum",
-        "email" => "203040089@mail.unpas.ac.id",
-        "image" => "a.jpg"
-    ]);
+  return view('about', [
+    "title" => "About",
+    "name" => "Bahrul Ulum",
+    "email" => "203040089@mail.unpas.ac.id",
+    "image" => "a.jpg"
+  ]);
 });
 
 
@@ -38,23 +38,22 @@ Route::get('/posts', [PostController::class, 'index']);
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('/categories', function () {
-    return view('categories', [
-        'title' => 'Post Categories',
-        'categories' => Category::all()
-    ]);
+  return view('categories', [
+    'title' => 'Post Categories',
+    'categories' => Category::all()
+  ]);
 });
 
 Route::get('/categories/{category:slug}', function (Category $category) {
-    return view('category', [
-        'title' => $category->name,
-        'posts' => $category->posts,
-        'category' => $category->name
-    ]);
+  return view('posts', [
+    'title' => "Post By Category : $category->name",
+    'posts' => $category->posts->load('category', 'author'),
+  ]);
 });
 
 Route::get('/authors/{author:username}', function (User $author) {
-    return view('posts', [
-        'title' => 'User Post',
-        'posts' => $author->posts,
-    ]);
+  return view('posts', [
+    'title' => "Post By Author : $author->name",
+    'posts' => $author->posts->load('category', 'author'),
+  ]);
 });
