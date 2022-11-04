295b57619d5d29dd044e871b0a4d97e9b3cbcba8
UlumBahrul <114681101+UlumBahrul@users.noreply.github.com>
2022-11-04 15:21:29 +0700

    video 12. redesign ui

--- laravel_203040089/resources/views/post.blade.php
+++ laravel_203040089/resources/views/post.blade.php
@@ -1,13 +1,24 @@
 @extends('layouts.main')
 
 @section('container')
-    <article class="mb-4">
-        <h2 class="mb-4">{{ $post->title }}</h2>
-        
-        <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
 
-        {!! $post->body !!}
-    </article>
+    <div class="container">
+        <div class="row justify-content-center mb-5" >
+            <div class="col-md-8">
+                <h2 class="mb-3">{{ $post->title }}</h2>
+                
+                <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
 
-    <a href="/posts" class="text-decoration-none" >Back to Posts</a>
+                <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid" >
+
+                <article class="my-3 fs-5">
+                    {!! $post->body !!}
+                </article>
+
+                <a href="/posts" class="text-decoration-none" >Back to Posts</a>
+            </div>
+        </div>
+    </div>
+
+    
 @endsection
