9bd95a8dfd1523cfbd7b8d1b6458551f3e18c8f3
UlumBahrul <203040089@mail.unpas.ac.id>
2022-11-06 00:48:10 +0700

    video 14. view login & registration

--- laravel_203040089/resources/views/layouts/main.blade.php
+++ laravel_203040089/resources/views/layouts/main.blade.php
@@ -4,8 +4,18 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title> ZIO Blog | {{ $title }} </title>
+
+    {{-- bootstrap css --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    
+    {{-- bootstrap icon --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+    {{-- my style --}}
+    <link rel="stylesheet" href="/css/style.css">
+
+    <title> ZIO Blog | {{ $title }} </title>
+    
 </head>
 
 <body>
